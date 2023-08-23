f115270e507e1e54a245da936d7dc0bbc037cf00
chocopie777 <egoran99@gmail.com>
2023-08-23 18:16:48 +0500

    started dev functional

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,15 @@
 const inpSearch = document.getElementById('inp-search');
 const btnSearch = document.getElementById('btn-search');
-
+let URL = "https://api.weatherapi.com/v1/current.json?key=c30ea8f295484f37b6d85901232308&q=";
 btnSearch.addEventListener('click', () => {
     const value = inpSearch.value;
     inpSearch.value = '';
+    let promise = fetch(URL.concat(value));
+    promise.then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data.current.temp_c);
+        console.log(data.current);
+        console.log(data);
+    })
 });
-//
