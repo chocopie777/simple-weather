0aa24f1e2e9a85e5599c8ee58a2ed7b2fd94bfe1
chocopie777 <egoran99@gmail.com>
2023-08-23 14:06:11 +0500

    finished styles

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+const inpSearch = document.getElementById('inp-search');
+const btnSearch = document.getElementById('btn-search');
+
+btnSearch.addEventListener('click', () => {
+    const value = inpSearch.value;
+    inpSearch.value = '';
+});
